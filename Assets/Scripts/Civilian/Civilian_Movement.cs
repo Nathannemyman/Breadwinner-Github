401c3bf1e40598096680ac8fb9970d4e4a02f1d0
Adam_Greene <greene.adam5135@gmail.com>
2025-02-12 23:22:48 -0500

    Added civilian random stops and random speeds

--- Assets/Scripts/Civilian/Civilian_Movement.cs
+++ Assets/Scripts/Civilian/Civilian_Movement.cs
@@ -1,17 +1,37 @@
-using System.Threading;
+using System.Collections;
 using UnityEngine;
 
 public class Civilian_Movement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private float speed;
+    private bool isMoving = true;
+    private const float check_movement_interval = 3f;
+
     void Start()
     {
-        
+        speed = Random.Range(4f, 8f);
+        StartCoroutine(Movement_Stops_Routine());
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.left * 5 * Time.deltaTime;
+        if (isMoving)
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+    }
+    IEnumerator Movement_Stops_Routine()
+    {
+        while (true)
+        {
+            // 50% chance to stop for 3 seconds, checked every 3 seconds
+            yield return new WaitForSeconds(check_movement_interval);
+            if (Random.Range(0f, 1f) < 0.5f) 
+            {
+                isMoving = false;
+                yield return new WaitForSeconds(3f);
+                isMoving = true;
+            }
+        }
     }
 }
